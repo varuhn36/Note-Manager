47b20de79af06fb6f8d630d73330790ad5afe2d0
varuhn36 <varuhn63@gmail.com>
2023-12-29 14:04:31 -0500

    Stop tracking README Images folder

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Notes Website
-Hello, this is my notes site
+# Note Manager
+Note Manager is a place for you to store all your notes safely. You can create notes, update and delete them as you wish, and your notes can be accessed anywhere, as long as you have an internet connection!
+
+# Landing Page
+When you first visit Note Manager, you will be brought here, where you can then login if you already have an account, or signup if you are new.
+![Landing Page](https://drive.google.com/file/d/1n2nDmnR3W0ABlDtbUSpTbV_ELnEGBQBv/view?usp=drive_link)
+
--- frontends/src/store.js
+++ frontends/src/store.js
@@ -1,8 +1,8 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux'
-import thunk from 'redux-thunk'
-import { composeWithDevTools} from 'redux-devtools-extension'
-import { userLoginReducer, userRegisterReducer, userUpdateReducer } from './reducers/userReducers'
-import { noteCreateReducer, noteDeleteReducer, noteListReducer, noteUpdateReducer } from './reducers/noteReducers'
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools} from 'redux-devtools-extension';
+import { userLoginReducer, userRegisterReducer, userUpdateReducer } from './reducers/userReducers';
+import { noteCreateReducer, noteDeleteReducer, noteListReducer, noteUpdateReducer } from './reducers/noteReducers';
 
 const reducer = combineReducers({
     userLogin: userLoginReducer,
@@ -12,15 +12,15 @@ const reducer = combineReducers({
     noteCreate: noteCreateReducer,
     noteUpdate: noteUpdateReducer,
     noteDelete: noteDeleteReducer,
-})
+});
 const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
 
 const firstState = {
     userLogin: {userInfo: userInfoFromStorage}
-}
+};
 
-const middleware = [thunk]
+const middleware = [thunk];
 
-const store = createStore(reducer, firstState, composeWithDevTools(applyMiddleware(...middleware)))
+const store = createStore(reducer, firstState, composeWithDevTools(applyMiddleware(...middleware)));
 
-export default store
+export default store;
